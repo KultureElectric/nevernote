7e8b06bd010007e7a724c9c3d21687efe6411250
ruben <rubenusshd@gmail.com>
2018-12-07 13:42:42 +0100

    applied all needed middlewares to create a log in session

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
 require("./models/userModel");
 require("./services/passport");
+const keys = require("./config/keys");
 
 mongoose.connect("mongodb://localhost/nevernote");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.cookieSessionKey]
+  })
+);
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
